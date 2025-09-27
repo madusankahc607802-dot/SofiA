ec12b0c9d2e0cf4fc750148713feff209646cfd5
madusankahc607802-dot <madusankahc607802@gmail.com>
2025-09-27 21:25:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
-BOT_TOKEN = os.getenv("7896090354:AAH3dJ4ytvmL3NkqAQYnUaPU4Od6yWjdQV4")  # Set this in Heroku Config Vars
+BOT_TOKEN = os.getenv("BOT_TOKEN")  # This reads the value of the environment variable BOT_TOKEN
 DEVELOPER = "@deweni2"
 SUPPORT_GROUP = "@slmusicmania"
